4bf9849c996d92850417abc90ff1be4a1b9c01e1
sava <petrovicsava3@gmail.com>
2024-10-01 21:33:31 +0200

    runnable

--- Backend/road-to-glory_be/src/app.module.ts
+++ Backend/road-to-glory_be/src/app.module.ts
@@ -2,30 +2,52 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PersistenceModule } from './persistence/persistence.module';
 import { CommunicationModule } from './communication/communication.module';
 import { GameObjectModule } from './game_object/game_object.module';
 import { MapModule } from './common/providers/map/map.module';
 import { UserService } from './common/providers/user/user.service';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
+import { JwtModule } from '@nestjs/jwt';
+import configuration from './persistence/configuration/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "src/common/environment/.env",
+      load: [configuration],
+    }),
     TypeOrmModule.forRoot({
     type: 'postgres',
     url: process.env.DATABASE_URL,
     autoLoadEntities:true,
     synchronize:true
   }),
+    AutomapperModule.forRoot({
+    strategyInitializer: classes(),
+  }),
+    JwtModule.registerAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      return {
+        secret: config.get("jwtSecret"),
+        signOptions: { expiresIn: "3600s" },
+      };
+    },
+    global: true,
+  }),
     AuthModule,
     PersistenceModule,
     CommunicationModule,
     GameObjectModule,
-    MapModule
+    MapModule,
+  
   ],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
